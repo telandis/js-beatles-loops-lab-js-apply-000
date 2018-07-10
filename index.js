780b6b7690f841c9de14dcae2b8437a3c8ef0ec7
Victror Zhu <victorzhu123@live.com>
2018-07-10 13:33:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var x = []
+  var i = 0
+  for (i = 0; i < musicians.length; i++) {
+    x.push(musicians[i] + " plays " + instruments[i])
+  }
+  return x
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+
+  while (facts[i]) {
+    facts[i] = facts[i] + "!!!"
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  var empty = []
+  do {
+    
+  }
+}
